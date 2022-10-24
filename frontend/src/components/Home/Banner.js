7691e519d3be654b2241bec03aacdf1c76f04149
nivrichter <nivrichter@gmail.com>
2022-10-24 22:46:48 +0300

    sdf

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
-  return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span id="get-part">A place to get</span>
-          <span> the cool stuff.</span>
-        </div>
-      </div>
-    </div>
-  );
-};
+const Banner = (props) => {
+    let [val, setVal] = useState("");
 
+    let changeHandler = (e) => {
+        setVal(e.target.value);
+        if (e.target.value.length >= 3) {
+            props.search(e.target.value);
+        } else {
+            props.search("");
+        }
+    };
+
+    return (
+        <div className="banner text-white">
+            <div className="container p-4 text-center">
+                <img src={logo} alt="banner" />
+                <div>
+                    <span id="get-part">A place to get</span>
+                    <input id="search-box" value={val} onChange={changeHandler}></input>
+                    <span> the cool stuff.</span>
+                </div>
+            </div>
+        </div>
+    );
+};
 export default Banner;
